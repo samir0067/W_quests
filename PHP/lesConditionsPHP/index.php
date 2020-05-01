e08f96031801ed900c9dab20d1b147007b6301c1
samir0067 <arrahmani.samir@gmail.com>
2020-05-01 15:23:17 +0200

    ajout de la quete Récupérer des informations avec SELECT

---
+++ PHP/lesConditionsPHP/index.php
@@ -0,0 +1,23 @@
+<?php
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Arrahmani" => [
+        "Strasbourg" => "Neudorf",
+        "Mitraillette" => "M4",
+        "Pistolet" => "G18"
+    ]
+];
+var_dump($characters);
+
